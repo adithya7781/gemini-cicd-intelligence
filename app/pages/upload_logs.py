8cde483f0148db265041ade5a9a60c2f303d5ae0
K.Vishal adithya <138603807+adithya7781@users.noreply.github.com>
2026-01-28 08:35:54 +0530

    Update upload_logs.py

--- app/pages/upload_logs.py
+++ app/pages/upload_logs.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sys
 import os
+import uuid
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 sys.path.append(ROOT_DIR)
@@ -10,17 +11,30 @@ from backend.log_ingestor import ingest_log
 
 def render():
 
-    st.title("📤 Upload CI/CD Log")
+    st.title("📤 Upload CI/CD Logs")
 
-    file = st.file_uploader("Upload pipeline log file", type=["log", "txt"])
+    uploaded_file = st.file_uploader(
+        "Upload pipeline log file",
+        type=["log", "txt", "csv", "xlsx", "pdf", "json"]
+    )
 
-    if file:
+    if uploaded_file:
 
-        save_path = "data/samples/uploaded.log"
+        os.makedirs("data/uploads", exist_ok=True)
+
+        file_extension = uploaded_file.name.split(".")[-1]
+
+        unique_name = f"{uuid.uuid4()}.{file_extension}"
+
+        save_path = os.path.join("data/uploads", unique_name)
 
         with open(save_path, "wb") as f:
-            f.write(file.read())
+            f.write(uploaded_file.read())
 
-        ingest_log(save_path)
+        if st.button("Process Log File"):
 
-        st.success("Log uploaded and processed successfully.")
+            with st.spinner("Parsing and storing log data..."):
+                run_id = ingest_log(save_path)
+
+            st.success("Log uploaded and processed successfully")
+            st.code(run_id)
