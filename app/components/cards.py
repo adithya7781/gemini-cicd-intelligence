cf06f6e5c1a16fb61fc531d7b6762e642311d5ac
K.Vishal adithya <138603807+adithya7781@users.noreply.github.com>
2026-01-29 10:22:46 +0530

    Update cards.py

--- app/components/cards.py
+++ app/components/cards.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def summary_cards(total_runs, failed_runs, avg_errors, critical_count):
+def summary_cards(total_runs, failed_runs, avg_errors, critical_count,severity_critical_count):
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
@@ -9,5 +9,6 @@ def summary_cards(total_runs, failed_runs, avg_errors, critical_count):
     col3.metric("Avg Errors", round(avg_errors, 2))
     col4.metric("Critical Alerts", critical_count)
     col5.metric("Severity Critical", severity_critical_count)
+
 
 
